be9a2de3e0e255567c8cb02430905828ce9055c4
analog-sombra <analogsombra@gmail.com>
2024-11-17 11:59:58 +0530

    fix : error fix

--- src/schemas/updatefile.ts
+++ src/schemas/updatefile.ts
@@ -12,37 +12,24 @@ import {
   pipe,
 } from "valibot";
 
-const UpdateFileSchema = pipe(
-  object({
-    file_no: pipe(string(), minLength(1, "Please enter file number.")),
-    applicant_name: pipe(
-      string(),
-      minLength(1, "Please enter applicant name.")
-    ),
-    survey_number: pipe(
-      string(),
-      minLength(1, "Please enter your file survey number.")
-    ),
-    villageId: pipe(number(), minValue(1, "Select village.")),
-    typeId: pipe(number(), minValue(1, "Select File Type.")),
-    names: optional(array(pipe(string(), minLength(1, "Please enter name.")))),
-    surveyNumbers: optional(
-      array(pipe(string(), minLength(1, "Please enter survey number.")))
-    ),
-    referenceNumbers: optional(
-      array(pipe(string(), minLength(1, "Please enter reference number.")))
-    ),
-    dates: optional(array(pipe(string(), minLength(1, "Please enter date.")))),
-  }),
-  forward(
-    check((input) => input.villageId != 0, "Select village."),
-    ["villageId"]
+const UpdateFileSchema = object({
+  file_no: pipe(string(), minLength(1, "Please enter file number.")),
+  applicant_name: pipe(string(), minLength(1, "Please enter applicant name.")),
+  survey_number: pipe(
+    string(),
+    minLength(1, "Please enter your file survey number.")
   ),
-  forward(
-    check((input) => input.typeId != 0, "Select File Type."),
-    ["typeId"]
-  )
-);
+  villageId: pipe(number(), minValue(1, "Select village.")),
+  typeId: pipe(number(), minValue(1, "Select File Type.")),
+  names: optional(array(pipe(string(), minLength(1, "Please enter name.")))),
+  surveyNumbers: optional(
+    array(pipe(string(), minLength(1, "Please enter survey number.")))
+  ),
+  referenceNumbers: optional(
+    array(pipe(string(), minLength(1, "Please enter reference number.")))
+  ),
+  dates: optional(array(pipe(string(), minLength(1, "Please enter date.")))),
+});
 
 type UpdateFileForm = InferInput<typeof UpdateFileSchema>;
 export { UpdateFileSchema, type UpdateFileForm };
