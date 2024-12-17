99653d5266ff1188d888ea0196ebd4f747c52f77
analog-sombra <analogsombra@gmail.com>
2024-12-17 15:28:06 +0530

    fix: bug fixed

--- src/schemas/file.ts
+++ src/schemas/file.ts
@@ -1,25 +1,35 @@
-import { InferInput, array, check, forward, minLength, minValue, number, object, string, pipe } from "valibot"
+import {
+  InferInput,
+  array,
+  check,
+  forward,
+  minLength,
+  minValue,
+  number,
+  object,
+  string,
+  pipe,
+} from "valibot";
 
-const FileSchema = pipe(object({
-    file_no: pipe(string(), minLength(1, "Please enter file number.")),
-    applicant_name: pipe(string(), minLength(1, "Please enter applicant name.")),
-    survey_number: pipe(string(), minLength(1, "Please enter your file survey number.") ,),
-    year: pipe(number(), minValue(1, "Please enter file year.")),
-    typeId: pipe(number(), minValue(1, "Select file type.")),
-    villageId: pipe(number(), minValue(1, "Select village.")),
-    names: array(pipe(string(), minLength(1, "Please enter name."))),
-    surveyNumbers: array(pipe(string(), minLength(1, "Please enter survey number."))),
-    referenceNumbers: array(
-      pipe(string(), minLength(1, "Please enter reference number."))
-    ),
-    dates: array(pipe(string(), minLength(1, "Please enter date."))),
-  }), forward(
-      check((input) => input.typeId != 0, "Select file type."),
-      ["typeId"]
-    ) , forward(
-      check((input) => input.villageId != 0, "Select village."),
-      ["villageId"]
-    ) ,);
+const FileSchema = object({
+  file_no: pipe(string(), minLength(1, "Please enter file number.")),
+  applicant_name: pipe(string(), minLength(1, "Please enter applicant name.")),
+  survey_number: pipe(
+    string(),
+    minLength(1, "Please enter your file survey number.")
+  ),
+  year: pipe(number(), minValue(1, "Please enter file year.")),
+  typeId: pipe(number(), minValue(1, "Select file type.")),
+  villageId: pipe(number(), minValue(1, "Select village.")),
+  names: array(pipe(string(), minLength(1, "Please enter name."))),
+  surveyNumbers: array(
+    pipe(string(), minLength(1, "Please enter survey number."))
+  ),
+  referenceNumbers: array(
+    pipe(string(), minLength(1, "Please enter reference number."))
+  ),
+  dates: array(pipe(string(), minLength(1, "Please enter date."))),
+});
 
 type FileForm = InferInput<typeof FileSchema>;
 export { FileSchema, type FileForm };
